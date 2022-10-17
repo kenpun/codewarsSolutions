0ec1fc9c0b5fa0349d51bdb12ec5238c380760f3
Kenneth Elumba <kennethelumba@gmail.com>
2022-10-17 14:02:09 +0200

    add solution for evenOrOdd kata

---
+++ evenOrOdd.js
@@ -0,0 +1,16 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" 
+// for odd numbers.
+
+function even_or_odd(number) {
+    if (number % 2 === 0) {
+     return "Even"
+   } else {
+     return "Odd"
+   }
+ }
+
+//  OR
+
+function even_or_odd(number) {
+    return number % 2 ? "Even" : "Odd";
+}
